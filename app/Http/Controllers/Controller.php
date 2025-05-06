db08ac6a0e2b7ae5775959b6405c5a3f752f1df6
Md Hafiz Al foisal <alfoisal940700@gmail.com>
2025-05-06 14:03:59 +0600

    api update

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,6 +4,6 @@ namespace App\Http\Controllers;
 
 abstract class Controller
 {
-    public $api = 'http://hotel.foisal/api';
-    // public $api = 'https://hotel.otitheesoftwaresolution.com/api';
+    // public $api = 'http://hotel.foisal/api';
+    public $api = 'https://hotel.otitheesoftwaresolution.com/api';
 }
